bef9c4b9e0cf0d2f4c0f89f414e6f5ed1e0f7cd8
user.email <younsoo6103@naver.com>
2025-06-11 15:40:08 +0900

    work

---
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -0,0 +1,20 @@
+package com.back.domain.post.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity //아래 구조대로 DB 테이블을 만들어야 한다.
+@Getter
+@Setter
+public class Post {
+    @Id //Primary Key
+    @GeneratedValue(strategy = IDENTITY) // Auti_increment
+    private int id; //int
+    private String title; //varchar(255)
+}
