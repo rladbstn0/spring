85096b6e520cd1b0b2a8e565755cf9e0507c73d8
user.email <younsoo6103@naver.com>
2025-06-12 15:17:53 +0900

    work

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -5,6 +5,7 @@ import com.back.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -20,5 +21,13 @@ public class PostService {
     }
     public Optional<Post> findById(int id) {
         return postRepository.findById(id);
+    }
+
+    public void modify(Post post, String title, String content) {
+        post.setTitle(title);
+        post.setContent(content);
+        post.setModifyDate(LocalDateTime.now());
+
+        postRepository.save(post); //UPDATE post SET title = "", content = "", modify_date = "" WHERE id = "";
     }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -3,9 +3,11 @@ package com.back.global.initData;
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -13,14 +15,18 @@ import java.util.Optional;
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
     private final PostService postService;
     private int callCount = 0;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
+            self.work3();
 
             callCount++;
         };
@@ -32,7 +38,7 @@ public class BaseInitData {
 
         Post post1 = new Post("제목1", "내용1"); // INSERT INTO post SET title = '제목 1';
         postService.save(post1);
-        Post post2 = postService.save(new Post("제목2", "내용2"));
+        Post post2 = postService.save(new Post("제목1", "내용2"));
 
         System.out.println("기본 데이터가 초기화되었습니다.");
 
@@ -45,5 +51,13 @@ public class BaseInitData {
         Post post1 = opPost1.get();
 
         System.out.println("post1 : "+ post1);
+    }
+
+    @Transactional
+    void work3() {
+        Optional<Post> opPost1 = postService.findById(1);
+        Post post1 = opPost1.get();
+
+        postService.modify(post1, "제목 1 수정", "내용 1 수정");
     }
 }
