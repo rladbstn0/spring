2d26c0cda41b62c4cfc60f2781686828ab1c19f5
user.email <younsoo6103@naver.com>
2025-06-12 14:14:05 +0900

    work

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity //아래 구조대로 DB 테이블을 만들어야 한다.
@@ -21,11 +23,15 @@ public class Post {
     @Id //Primary Key
     @GeneratedValue(strategy = IDENTITY) // Auto_increment
     private int id; //int
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private  String title; //varchar(255)
     @Column(columnDefinition = "TEXT")
     private  String content;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
