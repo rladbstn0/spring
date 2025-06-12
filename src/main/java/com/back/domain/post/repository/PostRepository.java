e85ca33df808a248ab0cf2ee3291c38949712c61
user.email <younsoo6103@naver.com>
2025-06-12 10:47:33 +0900

    work

---
+++ src/main/java/com/back/domain/post/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.back.domain.post.repository;
+
+
+import com.back.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer>{
+
+}
