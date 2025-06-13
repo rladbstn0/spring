e71b08d7aa4caeead537280f693ab0d38a1b634d
user.email <younsoo6103@naver.com>
2025-06-13 12:33:47 +0900

    work

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,39 +1,25 @@
 package com.back.domain.post.entity;
 
 
-import jakarta.persistence.*;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity //아래 구조대로 DB 테이블을 만들어야 한다.
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id //Primary Key
-    @GeneratedValue(strategy = IDENTITY) // Auto_increment
-    private int id; //int
-    @CreationTimestamp
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Post extends BaseEntity {
     private  String title; //varchar(255)
     @Column(columnDefinition = "TEXT")
     private  String content;
 
     public Post(String title, String content) {
-        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
---
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.back.global.jpa.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@MappedSuperclass // 엔티티의 부모 클래스는 이걸 달아야 한다
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id //Primary Ky
+    @GeneratedValue(strategy = IDENTITY) // Auto_increment
+    private int id;
+    @CreationTimestamp
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
