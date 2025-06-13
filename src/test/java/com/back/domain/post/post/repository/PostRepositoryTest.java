035b6e807a3ba2bdd8c3c642f461a7138e8bd100
user.email <younsoo6103@naver.com>
2025-06-13 14:42:31 +0900

    work

--- src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
+++ src/test/java/com/back/domain/post/post/repository/PostRepositoryTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 //@Rollback(false) //이게 없다면 Transaction에서 기본적으로 롤백
